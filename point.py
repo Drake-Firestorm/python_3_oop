70c43b5e2d7244b0d3fdcca8f192eb6e7caf749f
Drake-Firestorm <90408155+Drake-Firestorm@users.noreply.github.com>
2025-10-05 15:03:26 +0100

    feat: point
    add initialization function

--- point.py
+++ point.py
@@ -1,5 +1,9 @@
 import math
+
 class Point:
+    def __init__(self, x=0, y=0):
+        self.move(x, y)
+
     def move(self, x, y):
         self.x = x
         self.y = y
@@ -13,17 +17,6 @@ class Point:
             + (self.y - other_point.y) ** 2
         )
 
-# how to use it:
-point1 = Point()
-point2 = Point()
-
-point1.reset()
-point2.move(5, 0)
-
-print(point2.calculate_distance(point1))
-assert point2.calculate_distance(point1) == point1.calculate_distance(point2)
-
-point1.move(3, 4)
-
-print(point1.calculate_distance(point2))
-print(point1.calculate_distance(point1))
+# Constructing a Point
+point = Point(3, 4)
+print(point.x, point.y)
